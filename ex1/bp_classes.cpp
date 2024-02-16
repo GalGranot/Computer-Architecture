45f6381d7e4b775622a58798565b90d13701c9a4
GalGranot <gal.granot95@gmail.com>
2024-02-16 21:16:16 +0200

    added bp_classes
    divided the classes implementation into another file, started organizing functions

---
+++ ex1/bp_classes.cpp
@@ -0,0 +1,69 @@
+#include "bp_classes.h"
+
+/*=============================================================================
+* FSM
+=============================================================================*/
+void Fsm::updateState(bool taken)
+{
+	if (taken && state != STRONGLY_TAKEN)
+		state++;
+	else if (!taken && state != STRONGLY_NOT_TAKEN)
+		state--;
+}
+
+int Fsm::getState() { return state; }
+
+/*=============================================================================
+* BtbEntry
+=============================================================================*/
+void BtbEntry::updateHistory(bool taken)
+{
+	history <<= 1;
+	history += taken ? 1 : 0;
+}
+
+uint8_t BtbEntry::getHistory(int historySize)
+{
+	clearUpperHistoryBits();
+	return history;
+}
+
+void BtbEntry::clearUpperHistoryBits()
+{
+	int bitsToClear = MAX_HISTORY_SIZE - historySize;
+	int mask = (1 << bitsToClear) - 1;
+	history &= mask;
+}
+
+uint32_t BtbEntry::getTag() { return tag; }
+/*=============================================================================
+* Btb
+=============================================================================*/
+Btb::Btb(bool type, unsigned btbSize, bool share)
+	: type(type), share(share), btbSize(btbSize)
+{
+	entries.resize(btbSize);
+	for (int i = 0; i < btbSize; i++)
+		entries[i] = new BtbEntry();
+}
+
+Btb::~Btb()
+{
+	for (int i = 0; i < btbSize; i++)
+		delete entries[i];
+}
+
+BtbEntry& Btb::getEntry(const uint32_t tag)
+{
+	
+}
+
+void Btb::handleEntry(BtbEntry& entry)
+{
+
+}
+
+/*=============================================================================
+* BranchPredictor
+=============================================================================*/
+SIM_stats BranchPredictor::getStats() { return stats; }
